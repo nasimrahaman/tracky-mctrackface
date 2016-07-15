0415d3c91ba580dc714257f4de98bd9c8ffca260
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2016-07-15 15:32:21 +0200

    - added wrapper around ctrax matfiles

--- tmtf/sim.py
+++ tmtf/sim.py
@@ -6,6 +6,8 @@ import glob
 import numpy as np
 from scipy.ndimage import imread, gaussian_filter
 
+from Antipasti.netdatautils import ctrax2np
+
 
 class Simulator(object):
     def __init__(self):
@@ -50,11 +52,16 @@ class FlySimulator(Simulator):
         # problem doesn't define an episode (unlike in Atari games, where there's a game-start and game-over).
         # The environment should support arbitrary episodes as long as it's valid.
         self._episodeT = None
+        self._episodestart = None
+        self._episodestop = None
 
         # Parse directory
         self.filenames = None
         self.imshape = None
         self.parsedir()
+
+        # State tuple
+        self._state = None
 
         # Make a persistent crosshair and init with seed
         self.crosshair = self.seed
@@ -80,13 +87,19 @@ class FlySimulator(Simulator):
         assert value <= self._maxT
         self._episodeT = value
 
-    def initepisode(self):
+    def initepisode(self, episodestart, episodestop):
         """Initialize an episode."""
-        pass
+        assert self._minT <= episodestart < episodestop <= self._maxT
+
+        self.episodeT = self._episodestart = episodestart
+        self._episodestop = episodestop
 
     def getstate(self):
         # Fetch frames
-        pass
+        frames = self.fetchframes()
+        # Fetch crosshair image
+        crossimg = self.crosshair_image(imshape=self.imshape, coordinates=self.crosshair)
+        # TODO
 
     def resetenv(self):
         """Reset environment."""
@@ -97,8 +110,15 @@ class FlySimulator(Simulator):
         self.filenames = sorted(glob.glob("{}/*.png".format(self.path)), key=lambda x: int(x.split('.')[0]))
         self.imshape = imread(self.filenames[0]).shape
 
-    def fetchframes(self, stop, numsteps):
+    def fetchframes(self, stop=None, numsteps=None):
         """Fetch `numsteps` frames starting at `start` and concatenate along the 0-th axis."""
+
+        if stop is None:
+            stop = self.episodeT
+
+        if numsteps is None:
+            numsteps = self.framesperstate
+
         frames = np.array([imread(self.filenames[framenum])
                            for framenum in range(stop - numsteps + 1, stop + 1)])[::-1, ...]
         return frames
@@ -118,3 +138,46 @@ class FlySimulator(Simulator):
             crossimg = gaussian_filter(crossimg, sigma=smooth)
         # Done
         return crossimg
+
+
+class Track(object):
+    """Class to wrap ctrax MATLAB file."""
+    def __init__(self, matfilepath, objectid):
+        """
+        :type matfilepath: str
+        :param matfilepath: Path to matlab file.
+
+        :type objectid: int
+        :param objectid: ID of the object being tracked.
+        """
+
+        # Meta
+        self.matfilepath = matfilepath
+        self.objectid = objectid
+
+        # Parse matfile to a position array
+        self.posarray = ctrax2np(matpath=matfilepath)
+
+        assert self.objectid < self.posarray.shape[1], "Object ID does not correspond to an object in the ctrax file."
+
+    def getposition(self, T):
+        return self.posarray[T, self.objectid, :]
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            assert item.stop < self.posarray.shape[0]
+            assert item.start > 0
+        elif isinstance(item, int):
+            assert item < self.posarray.shape[0]
+            assert item > 0
+        else:
+            raise NotImplementedError("`item` must be a slice or an integer.")
+
+        # Get position
+        pos = self.getposition(item)
+
+        # Convert to a tuple if pos is just a 2-vector and return
+        if pos.ndim == 1:
+            return tuple(pos)
+        else:
+            return pos
